ab41335379e27d2087d1eb80ee44c392a3cb6065
suganparvathi <sugan@jojopays.com>
2023-02-01 15:40:09 +0530

    3:40  update

--- src/pages/SignUp/Components/Validation.tsx
+++ src/pages/SignUp/Components/Validation.tsx
@@ -32,6 +32,7 @@ import {BiUpload} from 'react-icons/bi'
 import { Axios } from "axios";
 import { ErrorMessage } from "formik";
 import { FaEye } from "react-icons/fa";
+import { AiFillEyeInvisible } from "react-icons/ai";
 
 const validationSchema = yup.object().shape({
   ValidationCode: yup
@@ -54,8 +55,7 @@ interface ValidationProps {
   ValidationCode: string;
   NewPassword:string;
   VerifyPassword: string;
- 
- 
+  
 }
 
 
@@ -127,16 +127,13 @@ const Validation = () => {
   };
 console.log("USERIID", userId?.data);
 
-const ValidationData = [
-  {name: "ValidationCode", placeholder:"ValidationCode", icon:<FaEye/> },
-  {name: "NewPassword", placeholder:"NewPassword", icon:<FaEye  type={icon1} onClick={handleToggle1}/>},
-  {name: "VerifyPassword", placeholder:"VerifyPassword", icon:<FaEye type={icon1} onClick={handleToggle1}/>},
- 
- 
-]
-{/* <FaEye type={icon1} onClick={handleToggle1}/> */}
+const[eye,seteye]=useState(false);
+const iconButton = () =>seteye(!eye);
+
+
+
  const renderError = (msg:string) => 
-      <div className='text-sm  w-full'>
+      <div className='text-sm -mt-4  w-full'>
         <p className='text-red-400'>{msg}</p>
       </div>
 
@@ -144,33 +141,55 @@ const ValidationData = [
   return (
 
     <div className="bg-white w-screen h-screen flex items-center justify-center">
-      <div className="flex justify-center w-[90%] h-[500px]  bg-[#F3F3FF] shadow rounded-xl overflow-hidden  bg-opacity-50 backdrop-blur">
+      <div className="flex justify-center w-[70%] h-[580px] bg-quaternary shadow rounded-xl overflow-hidden  bg-opacity-50 backdrop-blur">
         <div className="w-full col-span-1 flex items-center justify-center flex-col p-7">
-          <h5 className="text-black font-bold text-3xl capitalize mb-5">
+          <h5 className="text-black font-bold text-4xl  capitalize -mt-8">
            Forgot your password?
           </h5>
-          {!login ?
-          <div className="w-[980px] h-full bg-quaternary px-4 py-10 rounded-lg rounded-3xl shadow-md">
+        
+          <div className="w-[700px] h-[70%]  bg-[#F3F3FF] px-4 mt-10 py-10 rounded-3xl shadow-md">
           <CustomForm
             initialValues={initialState}
             validationSchema={validationSchema}
             onSubmit={handleSubmit}
           >
-            <div className="flex flex-col justify-center items-center mt-10  ml-14">
-                {ValidationData.map((data) => (
+            <div className="flex flex-col justify-center items-center mt-2">
+              <div className="flex flex-col">
+              <Input 
+                 type="text"
+                 name="ValidationCode"
+                 placeholder="Validation Code"
+                 className="border-none text-black outline-none bg-blue   shadow-md w-96" inputContainerClassName="mb-8"/>
+                 <ErrorMessage name="ValidationCode" render={renderError}/>
+                 
                   <div className="flex flex-row">
-                    <Input
-                    type="password"
-                    name={data.name}
-                    placeholder={data.placeholder}
-                    className="border-none text-black outline-none bg-blue   shadow-md w-96"
-                    inputContainerClassName="mb-6"
-                    
-                    />
-                   <p> {data.icon}</p>
-                    <ErrorMessage name={data.name} render={renderError}/>
-                </div>
-                  ))}
+                <Input 
+                  type={type1}
+                 name="NewPassword"
+                 placeholder="New Password"
+                
+                 className="border-none text-black outline-none bg-blue  shadow-md w-96" inputContainerClassName="mb-6"/>
+                 {" "}<span className="-ml-6">
+                  <FaEye className="mt-3 -ml-4" type={icon1} onClick={handleToggle1}/></span>
+                  </div>
+                <ErrorMessage className="-mt-10" name="NewPassword" render={renderError}/>
+                <div className="flex flex-row">
+                <Input 
+                  type={type2}
+                 name="VerifyPassword"
+                 placeholder="Verify Password"
+                
+                 className="border-none text-black outline-none bg-blue  shadow-md w-96" inputContainerClassName="mb-6"/>
+                 {" "}<span className="-ml-6">
+                 {eye? <FaEye className="mt-3 -ml-4" type={icon2} onClick={handleToggle2}/> : <AiFillEyeInvisible/>  } 
+                 {/* {icon? <FaUserAltSlash  />  :  <FaUserPlus/>}  */}
+                  </span>
+                  </div>
+                  <ErrorMessage name="VerifyPassword" render={renderError}/>
+              
+
+              </div>
+                
              
             </div>
             <div className="w-full flex justify-end">
@@ -178,22 +197,13 @@ const ValidationData = [
                 type="submit"
                 title="Next"
                 isLoading={isLoading}
-                className="bg-secondaryText text-white p-2 px-9 w-[20%] m-auto mt-6 transform transition-all hover:scale-95"
+                className="bg-secondaryText text-white p-2 px-7 w-[25%] m-auto mt-8 transform transition-all hover:scale-90"
               />
             </div>
           </CustomForm>
-          {error === "error" ?  
-          null
-          : 
-          <p className="text-red-200 w-full text-center mt-2">{`${error} already exist`}</p>
-        }
+         
             </div>
-          :
-          <div className="w-[500px] h-[10px] bg-white  rounded-lg shadow-md">
-            {/* {/ <CreateUser jojoUserId={userId} /> /} */}
-           </div>  
-          
-          }
+        
         </div>
         
       </div>
